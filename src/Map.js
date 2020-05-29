deeadb3f7101e2be21a8c19d493cb52ea1b36122
Judy <kimjudyh@gmail.com>
2020-05-28 17:45:39 -0700

    added google map with markers for each quake

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 
-class Map extends Component {
+const mapStyles = {
+  height: '100%',
+};
+
+const coords = {
+  lat: 37.78,
+  lng: -122.44
+}
+
+export class MapContainer extends Component {
   render() {
+    const markerList = this.props.quakeData.map((quake, index) => {
+      let lat = quake.geometry.coordinates[1];
+      let long = quake.geometry.coordinates[0];
+      let title = quake.properties.place;
+      return (<Marker position={{lat: lat, lng: long}} title={{title}} />)
+    })
+
     return (
-      <div>map</div>
+      <Map google={this.props.google}
+        zoom={1}
+        style={mapStyles}
+        containerStyle={{ height: '55%' }}
+        initialCenter={coords}>
+
+        {markerList}
+
+      </Map>
     )
   }
 }
 
-export default Map;
+// class Map extends Component {
+//   render() {
+//     return (
+//       <div>map</div>
+//     )
+//   }
+// }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg'
+})(MapContainer);
