7c3204152202aaf937cc988a0fec5885166fd0c5
Judy <kimjudyh@gmail.com>
2020-05-31 22:27:50 -0700

    changed map size, added infowindow

--- src/Map.js
+++ src/Map.js
@@ -11,15 +11,42 @@ const coords = {
 }
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    console.log('props', props);
+    console.log('marker', marker);
+    this.setState({
+      showingInfoWindow: true,
+      selectedPlace: props,
+      activeMarker: marker
+    });
+  }
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+      });
+    }
+  }
+
   render() {
     const markerList = this.props.quakeData.map((quake, index) => {
       let lat = quake.geometry.coordinates[1];
       let long = quake.geometry.coordinates[0];
       return (
-        <Marker 
+        <Marker
           position={{lat: lat, lng: long}} 
           icon={{url: "images/epicenter.png", scaledSize: new this.props.google.maps.Size(30,30)}}
-        />
+          onClick={this.onMarkerClick}
+          name={quake.properties.title}
+          >
+        </Marker> 
       )
     })
 
@@ -27,10 +54,20 @@ export class MapContainer extends Component {
       <Map google={this.props.google}
         zoom={1.1}
         style={mapStyles}
-        containerStyle={{ height: '45vh' }}
+        containerStyle={{ height: '55vh' }}
         initialCenter={coords}>
 
         {markerList}
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            {this.state.activeMarker.name}
+          </div>
+        </InfoWindow>
 
       </Map>
     )
